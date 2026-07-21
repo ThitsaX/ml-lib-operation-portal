c30cb67a647efb19de4b9a0ed08545f24534046b
nyeinnyeinei <nyein.ei@thitsaworks.com>
2026-07-21 13:22:59 +0900

    add usecase and api classes for Revenue Party

---
+++ implementation/uc_operation_portal/src/main/java/com/thitsaworks/operation_portal/usecase/operation_portal/impl/CreateRevenuePartyHandler.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2024-2026 ThitsaWorks Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thitsaworks.operation_portal.usecase.operation_portal.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thitsaworks.operation_portal.component.misc.annotation.ActionMetadata;
+import com.thitsaworks.operation_portal.component.misc.exception.DomainException;
+import com.thitsaworks.operation_portal.component.misc.util.ActionCategory;
+import com.thitsaworks.operation_portal.core.audit.command.CreateExceptionAuditCommand;
+import com.thitsaworks.operation_portal.core.audit.command.CreateInputAuditCommand;
+import com.thitsaworks.operation_portal.core.audit.command.CreateOutputAuditCommand;
+import com.thitsaworks.operation_portal.core.iam.cache.PrincipalCache;
+import com.thitsaworks.operation_portal.usecase.OperationPortalAuditableUseCase;
+import com.thitsaworks.operation_portal.usecase.util.action.ActionAuthorizationManager;
+import org.springframework.stereotype.Service;
+import com.thitsaworks.operation_portal.component.common.identifier.UserId;
+import com.thitsaworks.operation_portal.core.revenue_party.command.CreateRevenuePartyCommand;
+import com.thitsaworks.operation_portal.core.revenue_party.query.RevenuePartyQuery;
+import com.thitsaworks.operation_portal.usecase.operation_portal.CreateRevenueParty;
+import com.thitsaworks.operation_portal.usecase.util.UserPermissionManager;
+
+@Service
+@ActionMetadata(category = ActionCategory.REVENUE_PARTY)
+public class CreateRevenuePartyHandler
+    extends OperationPortalAuditableUseCase<CreateRevenueParty.Input, CreateRevenueParty.Output>
+    implements CreateRevenueParty {
+
+    private final CreateRevenuePartyCommand createRevenuePartyCommand;
+    private final RevenuePartyQuery revenuePartyQuery;
+    private final UserPermissionManager userPermissionManager;
+
+    public CreateRevenuePartyHandler(CreateInputAuditCommand createInputAuditCommand,
+                                     CreateOutputAuditCommand createOutputAuditCommand,
+                                     CreateExceptionAuditCommand createExceptionAuditCommand,
+                                     ObjectMapper objectMapper,
+                                     PrincipalCache principalCache,
+                                     ActionAuthorizationManager actionAuthorizationManager,
+                                     CreateRevenuePartyCommand createRevenuePartyCommand,
+                                     RevenuePartyQuery revenuePartyQuery,
+                                     UserPermissionManager userPermissionManager) {
+
+        super(createInputAuditCommand, createOutputAuditCommand, createExceptionAuditCommand,
+              objectMapper, principalCache, actionAuthorizationManager);
+
+        this.createRevenuePartyCommand = createRevenuePartyCommand;
+        this.revenuePartyQuery = revenuePartyQuery;
+        this.userPermissionManager = userPermissionManager;
+    }
+
+    @Override
+    protected Output onExecute(Input input) throws DomainException {
+
+        var currentUser = this.userPermissionManager.getCurrentUser();
+        var output = this.createRevenuePartyCommand.execute(new CreateRevenuePartyCommand.Input(
+            input.partyCode(), input.partyName(), input.partyType(), input.description(),
+            new UserId(currentUser.principalId().getId())));
+
+        return new Output(output.created(), output.revenuePartyId(), null,
+                          this.revenuePartyQuery.get(output.revenuePartyId()));
+    }
+
+}
+
---
+++ implementation/web_api_operation/src/main/java/com/thitsaworks/operation_portal/api/operation/portal/controller/revenueServices/GetRevenuePartyByIdController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2024-2026 ThitsaWorks Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thitsaworks.operation_portal.api.operation.portal.controller.revenueServices;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thitsaworks.operation_portal.component.common.identifier.RevenuePartyId;
+import com.thitsaworks.operation_portal.component.misc.exception.DomainException;
+import com.thitsaworks.operation_portal.core.revenue_party.data.RevenuePartyData;
+import com.thitsaworks.operation_portal.usecase.operation_portal.GetRevenuePartyById;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GetRevenuePartyByIdController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GetRevenuePartyByIdController.class);
+
+    private final GetRevenuePartyById getRevenuePartyById;
+    private final ObjectMapper objectMapper;
+
+    @GetMapping(value = "/secured/getRevenuePartyById")
+    public ResponseEntity<Response> execute(@RequestParam Long revenuePartyId)
+        throws DomainException, JsonProcessingException {
+
+        LOG.info("Get Revenue Party By Id Request : revenuePartyId= [{}]", revenuePartyId);
+
+        var output = this.getRevenuePartyById.execute(new GetRevenuePartyById.Input(
+            new RevenuePartyId(revenuePartyId)));
+        var response = new Response(output.revenueParty());
+
+        LOG.info("Get Revenue Party By Id Response : [{}]", this.objectMapper.writeValueAsString(response));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Response(@JsonProperty("revenueParty") RevenuePartyData revenueParty) { }
+
+}
