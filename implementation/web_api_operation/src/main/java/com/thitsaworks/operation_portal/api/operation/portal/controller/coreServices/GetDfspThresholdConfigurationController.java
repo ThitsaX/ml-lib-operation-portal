43ab1362f519e384b453e2cbd52430215f6db650
Nyi Nyi Sai <142963425+nyisai02@users.noreply.github.com>
2026-07-22 15:12:20 +0700

    Update GetDfspThresholdConfigurationController.java

--- implementation/web_api_operation/src/main/java/com/thitsaworks/operation_portal/api/operation/portal/controller/coreServices/GetDfspThresholdConfigurationController.java
+++ implementation/web_api_operation/src/main/java/com/thitsaworks/operation_portal/api/operation/portal/controller/coreServices/GetDfspThresholdConfigurationController.java
@@ -27,7 +27,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -40,8 +40,8 @@ public class GetDfspThresholdConfigurationController {
 
     private final ObjectMapper objectMapper;
 
-    @GetMapping("/secured/ndc/configurations/dfsp/{dfspId}")
-    public ResponseEntity<Response> execute(@PathVariable("dfspId") String dfspId)
+    @GetMapping("/secured/ndc/configurations/dfsp")
+    public ResponseEntity<Response> execute(@RequestParam("dfspId") String dfspId)
         throws DomainException, JsonProcessingException {
 
         LOG.info("Get NDC DFSP Threshold Configuration Request : dfspId=[{}]", dfspId);
