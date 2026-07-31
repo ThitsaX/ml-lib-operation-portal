dd72785786a5c89061d5c4d2c076956a519d35ef
Nyi Nyi Sai <142963425+nyisai02@users.noreply.github.com>
2026-07-31 15:09:21 +0700

    https://thitsa.atlassian.net/browse/MOJ-1197

--- implementation/component/mod_infra/src/main/resources/all/db/core/V1_40__add_user_visible_column.sql
+++ implementation/component/mod_infra/src/main/resources/all/db/core/V1_40__add_user_visible_column.sql
@@ -1,3 +1,6 @@
-ALTER TABLE tbl_user ADD COLUMN is_visible BOOLEAN NOT NULL DEFAULT TRUE;
+ALTER TABLE tbl_user
+ADD COLUMN is_visible TINYINT(1) NOT NULL DEFAULT 1;
 
-UPDATE tbl_user SET is_visible = FALSE WHERE LOWER(email) = 'systemadmin@thitsaworks.com';
+UPDATE tbl_user
+SET is_visible = 0
+WHERE LOWER(email) = 'systemadmin@thitsaworks.com';
