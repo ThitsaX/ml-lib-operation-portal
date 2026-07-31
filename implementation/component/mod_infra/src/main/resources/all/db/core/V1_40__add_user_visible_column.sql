bb5ad28ca660ba03db8f9d7755c70e6246a9476c
Nyi Nyi Sai <142963425+nyisai02@users.noreply.github.com>
2026-07-31 15:03:01 +0700

    Update V1_40__add_user_visible_column.sql

--- implementation/component/mod_infra/src/main/resources/all/db/core/V1_40__add_user_visible_column.sql
+++ implementation/component/mod_infra/src/main/resources/all/db/core/V1_40__add_user_visible_column.sql
@@ -1,5 +1,3 @@
 ALTER TABLE tbl_user ADD COLUMN is_visible BOOLEAN NOT NULL DEFAULT TRUE;
 
-UPDATE tbl_user SET is_visible = TRUE;
-
 UPDATE tbl_user SET is_visible = FALSE WHERE LOWER(email) = 'systemadmin@thitsaworks.com';
