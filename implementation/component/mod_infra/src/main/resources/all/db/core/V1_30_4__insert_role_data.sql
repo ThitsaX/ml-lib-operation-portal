362d38f0653cae6a034eecdf1b17523bc405fd6c
moenandiaung <moenandi.aung@thitsaworks.com>
2026-08-03 14:52:25 +0630

    Implement https://thitsa.atlassian.net/browse/LIB-560

--- implementation/component/mod_infra/src/main/resources/all/db/core/V1_30_4__insert_role_data.sql
+++ implementation/component/mod_infra/src/main/resources/all/db/core/V1_30_4__insert_role_data.sql
@@ -2,5 +2,5 @@ INSERT INTO tbl_role (role_id, name, active, is_dfsp, created_date, updated_date
 VALUES (8, 'REVENUE-Admin', 1, 0, 1784711336, 1784711336);
 
 INSERT INTO tbl_role (role_id, name, active, is_dfsp, created_date, updated_date)
-VALUES (9, 'REVENUE-USER', 1, 0, 1784711336, 1784711336);
+VALUES (9, 'REVENUE-User', 1, 0, 1784711336, 1784711336);
 
