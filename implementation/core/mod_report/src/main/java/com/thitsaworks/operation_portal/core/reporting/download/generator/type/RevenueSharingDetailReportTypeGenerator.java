a7f7516c736d6d8b532fa637f9fbe49cc49d5a16
moenandiaung <moenandi.aung@thitsaworks.com>
2026-08-03 12:16:51 +0630

    Refactor https://thitsa.atlassian.net/browse/LIB-620

---
+++ implementation/core/mod_report/src/main/java/com/thitsaworks/operation_portal/core/reporting/download/generator/type/RevenueSharingDetailReportTypeGenerator.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2024-2026 ThitsaWorks Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thitsaworks.operation_portal.core.reporting.download.generator.type;
+
+import com.thitsaworks.operation_portal.component.common.type.ReportType;
+import com.thitsaworks.operation_portal.core.reporting.download.generator.ReportGeneratedFile;
+import com.thitsaworks.operation_portal.core.reporting.download.generator.ReportGenerator;
+import com.thitsaworks.operation_portal.core.reporting.download.generator.ReportTypeGenerator;
+import com.thitsaworks.operation_portal.core.reporting.download.generator.support.ReportGeneratorSupport;
+import com.thitsaworks.operation_portal.core.reporting.download.model.ReportDownloadRequest;
+import com.thitsaworks.operation_portal.reporting.report.domain.GenerateRevenueSharingDetailReportCommand;
+import com.thitsaworks.operation_portal.reporting.report.exception.ReportErrors;
+import com.thitsaworks.operation_portal.reporting.report.exception.ReportException;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@Component
+@RequiredArgsConstructor
+class RevenueSharingDetailReportTypeGenerator implements ReportTypeGenerator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RevenueSharingDetailReportTypeGenerator.class);
+
+    private static final String FILE_TYPE_XLSX = "xlsx";
+
+    private static final int MAX_ROWS_PER_REPORT_FILE = 500_000;
+
+    private final GenerateRevenueSharingDetailReportCommand generateRevenueSharingDetailReportCommand;
+
+    private final ReportGeneratorSupport reportGeneratorSupport;
+
+    private final ReportGenerator.Settings settings;
+
+    @Override
+    public ReportType reportType() {
+
+        return ReportType.REVENUE_SHARING_DETAIL;
+    }
+
+    @Override
+    public ReportGeneratedFile generate(ReportDownloadRequest request, Map<String, String> params)
+        throws ReportException, IOException {
+
+        String fileType = this.reportGeneratorSupport.fileType(request.getFileType());
+        if (!FILE_TYPE_XLSX.equals(fileType)) {
+            throw new ReportException(ReportErrors.FILE_FORMAT_NOT_ALLOWED_EXCEPTION);
+        }
+
+        String settlementId = this.reportGeneratorSupport.requireParam(params, "settlementId");
+        String timezoneOffset = params.getOrDefault("timezoneOffset", "+0000");
+        String taxCode = this.reportGeneratorSupport.normalizeAllToken(params.getOrDefault("taxCode", "ALL"));
+        String category = this.reportGeneratorSupport.normalizeAllToken(params.getOrDefault("category", "ALL"));
+
+        int pageSize = this.settings.reportPageSize();
+        int totalRowCount = this.generateRevenueSharingDetailReportCommand.countRows(
+            new GenerateRevenueSharingDetailReportCommand.CountInput(settlementId, taxCode, category));
+        GenerateRevenueSharingDetailReportCommand.Input input =
+            new GenerateRevenueSharingDetailReportCommand.Input(
+                settlementId, fileType, timezoneOffset, taxCode, category, 0, pageSize);
+
+        if (totalRowCount <= pageSize) {
+            GenerateRevenueSharingDetailReportCommand.Output output =
+                this.generateRevenueSharingDetailReportCommand.execute(input);
+
+            return new ReportGeneratedFile(output.revenueSharingDetailRptByte(), FILE_TYPE_XLSX);
+        }
+
+        if (totalRowCount > MAX_ROWS_PER_REPORT_FILE) {
+            return this.generateSplitZip(
+                settlementId, fileType, timezoneOffset, taxCode, category, totalRowCount, pageSize);
+        }
+
+        GenerateRevenueSharingDetailReportCommand.Output output =
+            this.generateRevenueSharingDetailReportCommand.exportAll(input, totalRowCount, pageSize);
+
+        return new ReportGeneratedFile(output.revenueSharingDetailRptByte(), FILE_TYPE_XLSX);
+    }
+
+    private ReportGeneratedFile generateSplitZip(String settlementId,
+                                                 String fileType,
+                                                 String timezoneOffset,
+                                                 String taxCode,
+                                                 String category,
+                                                 int totalRowCount,
+                                                 int pageSize) throws ReportException, IOException {
+
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream)) {
+
+            int partNumber = 1;
+            for (int offset = 0; offset < totalRowCount; offset += MAX_ROWS_PER_REPORT_FILE) {
+                int rowsInPart = Math.min(MAX_ROWS_PER_REPORT_FILE, totalRowCount - offset);
+
+                GenerateRevenueSharingDetailReportCommand.Output partOutput =
+                    this.generateRevenueSharingDetailReportCommand.exportAll(
+                        new GenerateRevenueSharingDetailReportCommand.Input(
+                            settlementId,
+                            fileType,
+                            timezoneOffset,
+                            taxCode,
+                            category,
+                            offset,
+                            rowsInPart),
+                        rowsInPart,
+                        pageSize);
+
+                String entryName = "revenue_sharing_detail_part_" + partNumber + "." + fileType;
+                ZipEntry entry = new ZipEntry(entryName);
+                zipOutputStream.putNextEntry(entry);
+                zipOutputStream.write(partOutput.revenueSharingDetailRptByte());
+                zipOutputStream.closeEntry();
+                LOGGER.info(
+                    "Generated revenue sharing detail report part [{}] with [{}] rows",
+                    partNumber,
+                    rowsInPart);
+                partNumber++;
+            }
+
+            zipOutputStream.finish();
+            return new ReportGeneratedFile(byteArrayOutputStream.toByteArray(), "zip");
+        }
+    }
+
+}
---
+++ implementation/uc_operation_portal/src/main/java/com/thitsaworks/operation_portal/usecase/operation_portal/impl/GenerateRevenueSharingDetailReportHandler.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2024-2026 ThitsaWorks Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.thitsaworks.operation_portal.usecase.operation_portal.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thitsaworks.operation_portal.component.common.type.FileDownloadStatus;
+import com.thitsaworks.operation_portal.component.common.type.ReportType;
+import com.thitsaworks.operation_portal.component.misc.annotation.ActionMetadata;
+import com.thitsaworks.operation_portal.component.misc.exception.DomainException;
+import com.thitsaworks.operation_portal.component.misc.storage.S3FileStorage;
+import com.thitsaworks.operation_portal.component.misc.util.ActionCategory;
+import com.thitsaworks.operation_portal.core.audit.command.CreateExceptionAuditCommand;
+import com.thitsaworks.operation_portal.core.audit.command.CreateInputAuditCommand;
+import com.thitsaworks.operation_portal.core.audit.command.CreateOutputAuditCommand;
+import com.thitsaworks.operation_portal.core.iam.cache.PrincipalCache;
+import com.thitsaworks.operation_portal.core.reporting.download.request.ReportDownloadRequestManager;
+import com.thitsaworks.operation_portal.reporting.report.exception.ReportErrors;
+import com.thitsaworks.operation_portal.reporting.report.exception.ReportException;
+import com.thitsaworks.operation_portal.usecase.OperationPortalAuditableUseCase;
+import com.thitsaworks.operation_portal.usecase.operation_portal.GenerateRevenueSharingDetailReport;
+import com.thitsaworks.operation_portal.usecase.util.ReportDownloadUtil;
+import com.thitsaworks.operation_portal.usecase.util.action.ActionAuthorizationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@ActionMetadata(category = ActionCategory.REPORTING)
+public class GenerateRevenueSharingDetailReportHandler
+    extends OperationPortalAuditableUseCase<GenerateRevenueSharingDetailReport.Input, GenerateRevenueSharingDetailReport.Output>
+    implements GenerateRevenueSharingDetailReport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(
+        GenerateRevenueSharingDetailReportHandler.class);
+    private static final String ALL_FILTER = "ALL";
+
+    private final ReportDownloadRequestManager reportDownloadRequestManager;
+
+    private final S3FileStorage s3FileStorage;
+
+    public GenerateRevenueSharingDetailReportHandler(CreateInputAuditCommand createInputAuditCommand,
+                                                     CreateOutputAuditCommand createOutputAuditCommand,
+                                                     CreateExceptionAuditCommand createExceptionAuditCommand,
+                                                     ObjectMapper objectMapper,
+                                                     PrincipalCache principalCache,
+                                                     ActionAuthorizationManager actionAuthorizationManager,
+                                                     ReportDownloadRequestManager reportDownloadRequestManager,
+                                                     S3FileStorage s3FileStorage) {
+
+        super(
+            createInputAuditCommand, createOutputAuditCommand, createExceptionAuditCommand,
+            objectMapper, principalCache, actionAuthorizationManager);
+
+        this.reportDownloadRequestManager = reportDownloadRequestManager;
+        this.s3FileStorage = s3FileStorage;
+    }
+
+    @Override
+    protected Output onExecute(Input input) throws DomainException {
+
+        String normalizedFileType = ReportDownloadUtil.normalizeFileType(input.fileType());
+        if (!"xlsx".equals(normalizedFileType)) {
+            throw new ReportException(ReportErrors.FILE_FORMAT_NOT_ALLOWED_EXCEPTION);
+        }
+
+        Map<String, String> params = new HashMap<>();
+        params.put("settlementId", input.settlementId());
+        params.put("timezoneOffset", input.timezoneOffset());
+        params.put("taxCode", this.normalizeFilter(input.taxCode()));
+        params.put("category", this.normalizeFilter(input.category()));
+
+        ReportDownloadRequestManager.CreateOrReuseResult result = this.reportDownloadRequestManager.createPendingOrReuse(
+            ReportType.REVENUE_SHARING_DETAIL, normalizedFileType, params);
+
+        String fileKey = result.request().fileUrl();
+        String fileUrl = null;
+
+        if (FileDownloadStatus.READY.equals(result.request().status()) && fileKey != null &&
+                !fileKey.isBlank()) {
+
+            try {
+                fileUrl = this.s3FileStorage.generatePreSignedDownloadUrl(fileKey);
+            } catch (Exception e) {
+                LOG.warn(
+                    "Failed to generate pre-signed URL for requestId [{}]: [{}]",
+                    result.request().requestId().getEntityId(), e.getMessage());
+            }
+        }
+
+        if (FileDownloadStatus.FAILED.equals(result.request().status())) {
+            throw new ReportException(
+                ReportDownloadUtil.resolveFailedError(
+                    result.request().errorMessage(),
+                    ReportErrors.REVENUE_SHARING_DETAIL_REPORT_FAILURE_EXCEPTION));
+        }
+
+        return new Output(
+            result.request().requestId(), result.request().status(), fileUrl, fileKey,
+            result.paramsSignature());
+    }
+
+    private String normalizeFilter(String value) {
+
+        if (value == null || value.isBlank() || ALL_FILTER.equalsIgnoreCase(value.trim())) {
+            return ALL_FILTER;
+        }
+
+        return value.trim();
+    }
+
+}
